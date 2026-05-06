3e6ee876e051604d8baa3c5337a8cb227a5e4776
FGG Consultant <florent.gravin@gmail.com>
2026-05-06 10:38:38 +0200

    Fix orfan migrations

---
+++ geomanager/migrations/0056_merge_20260505_1349.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('geomanager', '0054_rastercoglayer'),
+        ('geomanager', '0055_wmslayer_popup'),
+    ]
+
+    operations = []
